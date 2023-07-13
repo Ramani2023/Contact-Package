7def1cb998f5b9de86e0d5ea8a4e190cbb48c9e2
Ramani2023 <ramanimoorthy45@gmail.com>
2023-07-13 17:38:38 -0500

    ad

--- src/views/contact.blade.php
+++ src/views/contact.blade.php
@@ -9,13 +9,14 @@
     <h1>Contact</h1>
 
 
-    {{ form_rest(form) }}
 
 <form action="{{route('contact')}}" method="post">
-    @csrf
+    
     <input type='text' name='name' placeholder="Your name Please"><br><br>
     <input type='email' name='email' placeholder="Your valid email"><br><br>
     <textarea name='message' cols='40' rows='20' placeholder="your qouery"></textarea><br><br>
+    <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+
     <button type='submit' value='Submit'>Submit</button>
 
 
